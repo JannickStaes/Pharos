a319c4037c4f5fff947c83cef94be23e0ed5dac7
Jannick Staes <staesjannick@gmail.com>
2019-12-28 17:14:28 +0100

    unicode2

--- Vin Web/Transactions/testdebugging.py
+++ Vin Web/Transactions/testdebugging.py
@@ -1,6 +1,6 @@
 from VinLibrary import *
 
-list_items = read_from_csv_keytrade('C:\\Users\\staes\\source\\repos\\JannickStaes\\Vin\\Vin Library\\Keytrade Download.csv')
+list_items = read_from_csv_keytrade('C:\\Users\\staes\\source\\repos\\JannickStaes\\Vin\\Vin Library\\Keytrade Download2.csv')
 print(list_items[0])
 for item in range(1):
         t = Transaction(amount = item['Amount'],
